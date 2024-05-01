971e32d13eb891cc81182c9b831354f81f349591
bigduu <mugeng.du@qq.com>
2024-05-02 01:20:17 +0800

    use flash motion instead of leap motion

--- lua/community.lua
+++ lua/community.lua
@@ -10,7 +10,7 @@ return {
   { import = "astrocommunity.pack.bash" },
   { import = "astrocommunity.pack.yaml" },
   { import = "astrocommunity.pack.typescript-all-in-one" },
-  { import = "astrocommunity.motion.leap-nvim" },
+  { import = "astrocommunity.motion.flash-nvim" },
   { import = "astrocommunity.motion.mini-move" },
   { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.debugging.telescope-dap-nvim" },
